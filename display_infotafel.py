4e27a967164fd6ee3acc34e737d03ca60df15267
cptke <git@paradx.ch>
2026-04-20 14:49:59 +0200

    refactor(display): read file directly, no http, no Clear

--- display_infotafel.py
+++ display_infotafel.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 import sys
 import time
-import urllib.request
 from PIL import Image
 
 sys.path.append('/home/ke/e-Paper/RaspberryPi_JetsonNano/python/lib')
 from waveshare_epd import epd7in5_V2
 
-IMAGE_URL   = 'http://localhost:8080/sbb.png'
-LOCAL_IMAGE = '/tmp/sbb_display.png'
+IMAGE_PATH = '/home/ke/sbb-infotafel-xlingen2/sbb.png'
 
 epd = epd7in5_V2.EPD()
 epd.init()
-epd.Clear()
 
 while True:
-    urllib.request.urlretrieve(IMAGE_URL, LOCAL_IMAGE)
-    img = Image.open(LOCAL_IMAGE).convert('RGB').convert('L').point(lambda x: 255 if x >= 200 else 0, '1')
+    img = Image.open(IMAGE_PATH).convert('RGB').convert('L').point(lambda x: 255 if x >= 200 else 0, '1')
     epd.display(epd.getbuffer(img))
     print(f'[{time.strftime("%H:%M:%S")}] ok', flush=True)
     time.sleep(60)
